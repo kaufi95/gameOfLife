a9351470a3b5a8cccd5b45424e3db480132e6b61
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2023-04-17 22:43:47 +0200

    v1.0

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# https://stackoverflow.com/questions/55408277/pygame-best-way-to-implement-buttons
-
 import pygame
 import numpy
-from button import Button
+import json
+import pygame_widgets
+from pygame_widgets.button import Button
 
 BLACK = (0, 0, 0)
 GRAY = (100, 100, 100)
@@ -23,9 +23,8 @@ clock = pygame.time.Clock()
 generation = 0
 speed = 10
 
-
 def main():
-    global grid, board, controls, speed, running, clock
+    global grid, board_surface, control_surface, speed, running, clock
     pygame.init()
     pygame.display.set_caption("Game Of Life")
 
@@ -33,23 +32,20 @@ def main():
         (WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
 
     # make the surface for the grid
-    board = screen.subsurface(
+    board_surface = screen.subsurface(
         (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT - CONTROLS_HEIGHT))
-    board.fill(BLACK)
+    board_surface.fill(BLACK)
 
     # make the surface for the controls
-    controls = screen.subsurface(
+    control_surface = screen.subsurface(
         (0, WINDOW_HEIGHT - CONTROLS_HEIGHT), (WINDOW_WIDTH, CONTROLS_HEIGHT))
-    controls.fill((0, 50, 0))
-
-    draw_controls()
 
     grid = numpy.zeros((COLS, ROWS))
 
     while True:
         update_game()
-
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
@@ -66,9 +62,10 @@ def main():
                             grid[x][y] = 1
                         else:
                             grid[x][y] = 0
-
+        
+        draw_controls()
+        pygame_widgets.update(events)
         pygame.display.update()
-
 
 def update_game():
     global grid, generation, speed, running, clock
@@ -77,8 +74,6 @@ def update_game():
         generation += 1
         grid = update_grid()
     drawGrid()
-    draw_information()
-
 
 def update_grid():
     global grid
@@ -111,99 +106,82 @@ def update_grid():
 
 
 def drawGrid():
-    global grid, board
-    board.fill(BLACK)
+    global grid, board_surface
+    board_surface.fill(BLACK)
     for x in range(COLS):
         for y in range(ROWS):
             rect = pygame.Rect(x*BLOCKSIZE, y*BLOCKSIZE, BLOCKSIZE, BLOCKSIZE)
             if grid[x][y] == 1:
-                pygame.draw.rect(board, YELLOW, rect)
+                pygame.draw.rect(board_surface, YELLOW, rect)
             else:
-                pygame.draw.rect(board, GRAY, rect, 1)
+                pygame.draw.rect(board_surface, GRAY, rect, 1)
 
-def draw_information():
+
+def draw_controls():
     global generation, speed
+    control_surface.fill((0, 50, 0))
     font = pygame.font.SysFont('Arial', 16)
     gen_text = font.render("Generation: " + str(generation), True, WHITE)
     speed_text = font.render("Speed: " + str(speed), True, WHITE)
 
-    controls.d.blit(gen_text, (20, 15))
-    controls.blit(speed_text, (WINDOW_WIDTH - 100, 15))
+    control_surface.blit(gen_text, (20, 15))
+    control_surface.blit(speed_text, (WINDOW_WIDTH - 100, 15))
 
-def draw_controls():
-    global bstart, bstop, bspeedp, bspeedn
-
-    bstart = Button(110, 40 , 80, 40, (200, 200, 0), (100, 100, 0), 'start')
-    bstart.draw(controls)
-    bstart.clicked = start()
-
-    # bstart = drawButton('start', 80, 40)
-    # controls.blit(bstart, (110, 40))
-
-    bstop = drawButton('stop', 80, 40)
-    controls.blit(bstop, (210, 40))
-
-    bsave = drawButton('save', 80, 40)
-    controls.blit(bsave, (310, 40))
-
-    bload = drawButton('load', 80, 40)
-    controls.blit(bload, (410, 40))
-
-    bspeedp = drawButton('+', 40, 40)
-    controls.blit(bspeedp, (510, 40))
-
-    bspeedn = drawButton('-', 40, 40)
-    controls.blit(bspeedn, (570, 40))
-
-
-def drawButton(name, x, y):
-    button_surface = pygame.Surface((x, y))
-    button_surface.fill(YELLOW)
-    font = pygame.font.SysFont('Arial', 12)
-    text_surface = font.render(name, True, (0, 0, 0))
-    button_surface.blit(text_surface, (
-        button_surface.get_rect().centerx - text_surface.get_rect().centerx,
-        button_surface.get_rect().centery - text_surface.get_rect().centery
-    ))
-    return button_surface
-
-
-def start():
     global running
-    running = True
-    print("start")
+
+    text = 'Start' if not running else 'Stop'
+    Button(control_surface, 110, 40, 80, 40, text=text, fontSize=16, inactiveColour=YELLOW, pressedColour=GRAY, onClick=play_or_pause)
+
+    Button(control_surface, 210, 40, 80, 40, text='Reset', fontSize=16, inactiveColour=YELLOW, pressedColour=GRAY, onClick=reset_grid)
+
+    Button(control_surface, 310, 40, 80, 40, text='Save', fontSize=16, inactiveColour=YELLOW, pressedColour=GRAY, onClick=save_grid)
+
+    Button(control_surface, 410, 40, 80, 40, text='Load', fontSize=16, inactiveColour=YELLOW, pressedColour=GRAY, onClick=load_grid)
+
+    Button(control_surface, 510, 40, 40, 40, text='+', fontSize=16, inactiveColour=YELLOW, pressedColour=GRAY, onClick=lambda: change_speed(1))
+
+    Button(control_surface, 570, 40, 40, 40, text='-', fontSize=16, inactiveColour=YELLOW, pressedColour=GRAY, onClick=lambda: change_speed(-1))
 
 
-def stop():
+def reset_grid():
+    global grid, generation
+    grid = numpy.zeros((COLS, ROWS))
+    generation = 0
+
+
+def play_or_pause():
     global running
-    running = False
-    print("stop")
+    if not running:
+        running = True
+        print("play")
+    else:
+        running = False
+        print("pause")
 
 
 def change_speed(value):
     global speed
+    print("speed: " + str(value))
     if value == 1:
-        speed += 5
+        speed += 1
     elif value == -1:
-        if speed > 5:
-            speed -= 5
+        if speed > 3:
+            speed -= 1
 
 
-def saveGrid():
+def save_grid():
     global grid
-    # save_file = open("gridData.json", "w")
-    # json.dump
-    # save grid to file
-    pass
+    json_string = json.dumps(grid.tolist())
+    with open('data.json', 'w', encoding='utf-8') as f:
+        f.write(json_string)
 
 
-def loadGrid():
+def load_grid():
     global grid
-#     import json
-# with open('data.json', 'w', encoding='utf-8') as f:
-#     json.dump(data, f, ensure_ascii=False, indent=4)
-    # load grid from file and write to grid
-    pass
+    with open('data.json', 'r', encoding='utf-8') as f:
+        json_string = f.read()
+        list = json.loads(json_string)
+        grid = numpy.array(list)
 
 
 if __name__ == "__main__":
